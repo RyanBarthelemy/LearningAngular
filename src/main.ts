a8eb0b4977f6edd86a21061725483f4eac4a87c0
axlor <axlor852@gmail.com>
2020-06-05 15:19:37 -0500

    Added Hammer.JS to the project: https://hammerjs.github.io/getting-started/ Added Material Icons to the project in index.html <head> tag.
    Pretty much setup now and ready to go.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import 'hammerjs'; /* https://hammerjs.github.io/getting-started/ */
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
