5c9779f059939069924b1e130b81969e35bee572
axlor <axlor852@gmail.com>
2020-06-05 15:09:19 -0500

    Created new hotel management/booking project called hotel-axlor.
    Added Angular Material and CDK to the project.
    
    Imported BrowserAnimationModule to app.module.ts so we have access to that.
    Since I'll probably be using a lot of Material modules, rather than clutter up app.module.ts imports array with tons of stuff, I made a new file to contain the Material components I end up using.
    
    Changed styles.css: added a prebuilt theme that will probably be changed later, just testing for now.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule /*All the Angular Material stuff we are going to use, see material.module.ts*/
   ],
   providers: [],
   bootstrap: [AppComponent]
