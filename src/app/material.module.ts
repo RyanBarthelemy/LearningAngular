0cb450807e3d8626fc2898df418a0868d9b46cae
axlor <axlor852@gmail.com>
2020-06-06 00:37:23 -0500

    Added Angular Material Button and Badge, testing with app component template

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 
+/* https://material.angular.io/components/categories */
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatButtonModule} from '@angular/material/button';
+
 @NgModule({
-  imports: [],
-  exports: []
+  imports: [MatBadgeModule, MatButtonModule],
+  exports: [MatBadgeModule, MatButtonModule]
 })
 
 export class MaterialModule{}
