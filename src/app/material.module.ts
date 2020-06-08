e693cc553a2d94950baf8763049d525579f5fcfe
axlor <axlor852@gmail.com>
2020-06-08 00:09:30 -0500

    Added open/close toggle for the nav bar.

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -6,6 +6,7 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
   imports: [
@@ -14,6 +15,7 @@ import {MatSidenavModule} from '@angular/material/sidenav';
     MatToolbarModule,
     MatIconModule,
     MatSidenavModule,
+    MatListModule,
   ],
   exports: [
     MatBadgeModule,
@@ -21,6 +23,7 @@ import {MatSidenavModule} from '@angular/material/sidenav';
     MatToolbarModule,
     MatIconModule,
     MatSidenavModule,
+    MatListModule,
   ]
 })
 
